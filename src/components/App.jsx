73887a780bba53a3e353e60b39657910b98c970c
Dylan Lewis and Nathan Stewart <nathan@>
2017-09-13 15:59:02 -0700

    change title, remove some elements

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,11 +3,7 @@ import React from "react";
 function App(props){
   return (
     <div>
-      <h1>Help Queue</h1>
       <h3>Waddup</h3>
-      <h3>Thato and Haley</h3>
-      <p><em>Firebase won't save record</em></p>
-      <hr/>
     </div>
   );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -54,7 +54,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template:'template.ejs',
       appMountId: 'react-app-root',
-      title: 'React Help Queue',
+      title: 'React Build Template',
       filename: resolve(__dirname, "build", "index.html"),
     }),
   ]
